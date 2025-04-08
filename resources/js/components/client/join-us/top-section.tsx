21667f93c48001b5347c14424e70729dfdc2de7c
mdsaifurrahmann <34965777+mdsaifurrahmann@users.noreply.github.com>
2025-04-08 20:02:11 +0000

    fix code style

--- resources/js/components/client/join-us/top-section.tsx
+++ resources/js/components/client/join-us/top-section.tsx
@@ -8,13 +8,18 @@ const TopSection = () => {
                     <div className="flex flex-col items-center justify-between gap-12 lg:flex-row lg:items-start">
                         <h2 className="min-w-fit text-4xl">join us</h2>
                         <div className="font-ysabeau px-2 text-center text-xl lg:ml-20 lg:px-0 lg:text-right">
-                            <p>Be part of a world-class cultural festival celebrating Scarborough’s rich diversity through music, art, and community! </p>
+                            <p>
+                                Be part of a world-class cultural festival celebrating Scarborough’s rich diversity through music, art, and
+                                community!{' '}
+                            </p>
 
-                            <p>We are currently accepting <span className="bg-primary rounded-sm py-px">applications for vendors for Scarborough Folk Fest 2025.</span></p>
+                            <p>
+                                We are currently accepting{' '}
+                                <span className="bg-primary rounded-sm py-px">applications for vendors for Scarborough Folk Fest 2025.</span>
+                            </p>
                             <p>Artist and sponsor applications will open soon.</p>
 
                             <p>Don’t miss your chance to get involved — stay tuned for updates!</p>
-
                         </div>
                     </div>
                     <div className="mt-10">
--- resources/js/components/client/join-us/vendor-form/vendor-form.tsx
+++ resources/js/components/client/join-us/vendor-form/vendor-form.tsx
@@ -138,7 +138,7 @@ const VendorForm = ({ success, error }: VendorFormProps) => {
                     type="text"
                     name="business_name"
                     id="business_name"
-                    className="bg-darkwhite focus:outline-primary mb-3 rounded-sm px-4 py-2 font-ysabeau"
+                    className="bg-darkwhite focus:outline-primary font-ysabeau mb-3 rounded-sm px-4 py-2"
                     value={data.business_name}
                     onChange={(e) => setData('business_name', e.target.value)}
                     placeholder="Business Name"
@@ -152,7 +152,7 @@ const VendorForm = ({ success, error }: VendorFormProps) => {
                     type="text"
                     name="contact_person"
                     id="contact_person"
-                    className="bg-darkwhite focus:outline-primary mb-3 rounded-sm px-4 py-2  font-ysabeau"
+                    className="bg-darkwhite focus:outline-primary font-ysabeau mb-3 rounded-sm px-4 py-2"
                     value={data.contact_person}
                     onChange={(e) => setData('contact_person', e.target.value)}
                     placeholder="Contact Person"
@@ -166,7 +166,7 @@ const VendorForm = ({ success, error }: VendorFormProps) => {
                     type="text"
                     name="phone_number"
                     id="phone_number"
-                    className="bg-darkwhite focus:outline-primary mb-3 rounded-sm px-4 py-2  font-ysabeau"
+                    className="bg-darkwhite focus:outline-primary font-ysabeau mb-3 rounded-sm px-4 py-2"
                     value={data.phone_number}
                     onChange={(e) => setData('phone_number', e.target.value)}
                     placeholder="Phone Number"
@@ -180,7 +180,7 @@ const VendorForm = ({ success, error }: VendorFormProps) => {
                     type="email"
                     name="email"
                     id="email"
-                    className="bg-darkwhite focus:outline-primary mb-3 rounded-sm px-4 py-2  font-ysabeau"
+                    className="bg-darkwhite focus:outline-primary font-ysabeau mb-3 rounded-sm px-4 py-2"
                     value={data.email}
                     onChange={(e) => setData('email', e.target.value)}
                     placeholder="Email"
@@ -194,7 +194,7 @@ const VendorForm = ({ success, error }: VendorFormProps) => {
                     type="text"
                     name="business_address"
                     id="business_address"
-                    className="bg-darkwhite focus:outline-primary mb-3 rounded-sm px-4 py-2 font-ysabeau"
+                    className="bg-darkwhite focus:outline-primary font-ysabeau mb-3 rounded-sm px-4 py-2"
                     value={data.business_address}
                     onChange={(e) => setData('business_address', e.target.value)}
                     placeholder="Business Address"
@@ -206,7 +206,7 @@ const VendorForm = ({ success, error }: VendorFormProps) => {
                     type="text"
                     name="web_media"
                     id="web_media"
-                    className="bg-darkwhite focus:outline-primary mb-3 rounded-sm px-4 py-2 font-ysabeau"
+                    className="bg-darkwhite focus:outline-primary font-ysabeau mb-3 rounded-sm px-4 py-2"
                     value={data.web_media}
                     onChange={(e) => setData('web_media', e.target.value)}
                     placeholder="https://example.com"
@@ -252,7 +252,7 @@ const VendorForm = ({ success, error }: VendorFormProps) => {
                             type="text"
                             name="vendor_other"
                             id="vendor_other"
-                            className="bg-darkwhite focus:outline-primary mb-3 rounded-sm px-4 py-2  font-ysabeau"
+                            className="bg-darkwhite focus:outline-primary font-ysabeau mb-3 rounded-sm px-4 py-2"
                             value={data.vendor_other}
                             onChange={(e) => setData('vendor_other', e.target.value)}
                             placeholder="Footware"
@@ -271,7 +271,7 @@ const VendorForm = ({ success, error }: VendorFormProps) => {
                 </label>
                 <textarea
                     name="product_services_desc"
-                    className="bg-darkwhite focus:outline-primary mb-3 rounded-sm px-4 py-2 font-ysabeau"
+                    className="bg-darkwhite focus:outline-primary font-ysabeau mb-3 rounded-sm px-4 py-2"
                     value={data.product_services_desc}
                     onChange={(e) => setData('product_services_desc', e.target.value)}
                 />
@@ -287,7 +287,7 @@ const VendorForm = ({ success, error }: VendorFormProps) => {
                             step={0.01}
                             name="min_price"
                             id="min_price"
-                            className="bg-darkwhite focus:outline-primary mb-3 rounded-sm px-4 py-2 font-ysabeau"
+                            className="bg-darkwhite focus:outline-primary font-ysabeau mb-3 rounded-sm px-4 py-2"
                             value={data.min_price}
                             onChange={(e) => setData('min_price', parseFloat(e.target.value))}
                             placeholder="Minimum"
@@ -302,7 +302,7 @@ const VendorForm = ({ success, error }: VendorFormProps) => {
                             step={0.01}
                             name="max_price"
                             id="max_price"
-                            className="bg-darkwhite focus:outline-primary mb-3 rounded-sm px-4 py-2 font-ysabeau"
+                            className="bg-darkwhite focus:outline-primary font-ysabeau mb-3 rounded-sm px-4 py-2"
                             value={data.max_price}
                             onChange={(e) => setData('max_price', parseFloat(e.target.value))}
                             placeholder="Maximum"
@@ -376,7 +376,7 @@ const VendorForm = ({ success, error }: VendorFormProps) => {
                     onChange={handleFileChange}
                     name="vendor_permit_copy"
                     id="vendor_permit_copy"
-                    className="bg-offwhite focus:outline-primary mb-3 rounded-sm border-none font-ysabeau"
+                    className="bg-offwhite focus:outline-primary font-ysabeau mb-3 rounded-sm border-none"
                     accept=".pdf,.jpg,.jpeg,.png,.doc,.docx"
                 />
                 <InputError className="mb-3" message={errors.vendor_permit_copy} />
@@ -393,7 +393,7 @@ const VendorForm = ({ success, error }: VendorFormProps) => {
                     type="text"
                     name="space"
                     id="space"
-                    className="bg-darkwhite focus:outline-primary mb-3 rounded-sm px-4 py-2 font-ysabeau"
+                    className="bg-darkwhite focus:outline-primary font-ysabeau mb-3 rounded-sm px-4 py-2"
                     value={data.space}
                     onChange={(e) => setData('space', e.target.value)}
                     placeholder="10’ x 10’"
@@ -425,7 +425,7 @@ const VendorForm = ({ success, error }: VendorFormProps) => {
                             type="text"
                             name="electricity_power"
                             id="electricity_power"
-                            className="bg-darkwhite focus:outline-primary mb-3 rounded-sm px-4 py-2 font-ysabeau"
+                            className="bg-darkwhite focus:outline-primary font-ysabeau mb-3 rounded-sm px-4 py-2"
                             value={data.electricity_power}
                             onChange={(e) => setData('electricity_power', e.target.value)}
                             placeholder="220V"
@@ -477,7 +477,7 @@ const VendorForm = ({ success, error }: VendorFormProps) => {
                     type="text"
                     name="special_request"
                     id="special_request"
-                    className="bg-darkwhite focus:outline-primary mb-3 rounded-sm px-4 py-2 font-ysabeau"
+                    className="bg-darkwhite focus:outline-primary font-ysabeau mb-3 rounded-sm px-4 py-2"
                     value={data.special_request}
                     onChange={(e) => setData('special_request', e.target.value)}
                 />
@@ -487,13 +487,13 @@ const VendorForm = ({ success, error }: VendorFormProps) => {
                     <Checkbox
                         name="agreement"
                         id="agreement"
-                        className="mb-3 mt-2"
+                        className="mt-2 mb-3"
                         checked={data.agreement}
                         onCheckedChange={(checked) => setData('agreement', checked as boolean)}
                     />
 
                     <div className="grid gap-1.5 leading-none">
-                        <label htmlFor="agreement" className="peer-disabled:cursor-not-allowed peer-disabled:opacity-70 font-ysabeau text-lg">
+                        <label htmlFor="agreement" className="font-ysabeau text-lg peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
                             By submitting this application, I understand that acceptance as a vendor for the Scarborough Folk Fest 2025 is not
                             guaranteed. If selected, I agree to comply with all event guidelines and regulations.
                         </label>
